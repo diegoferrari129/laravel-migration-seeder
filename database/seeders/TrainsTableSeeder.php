fa476bd3eae3b16e597156665571762335378c64
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-26 17:17:28 +0200

    [FEAT] created new seeder TrainsTableSeeder

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+use App\Model\Trains;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<10; $i++){
+            
+
+            $train->save();
+        }
+    }
+}
